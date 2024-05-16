a56251bdea2a3cc7b738ca64a8038c89c2b5efb2
Atsushi Kinoshita <atsumo@outlook.com>
2024-05-16 18:31:34 -0400

    Add gameplay and animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRB;
+    private Animator playerAnim;
+    public float jumpForce = 10f;
+    public float gravityModifier = 1f;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) & isOnGround & !gameOver)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true ;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true ;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+    }
+
+}
